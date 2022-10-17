0ac56a3ad6752a91c518678f84df14574ac80a7c
Arterixs <artemontugg@gmail.com>
2022-10-18 02:18:19 +0300

    feat: add solution repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!("separator" in options)) {
+    options.separator = "+"
+  }
+  if (!("additionSeparator" in options)) {
+    options.additionSeparator = "|"
+  }
+  if (!("repeatTimes" in options)) {
+    options.repeatTimes = 1
+  }
+  if (!("additionRepeatTimes" in options)) {
+    options.additionRepeatTimes = 1
+  }
+  let result = ""
+  let add = ""
+
+  if ("addition" in options) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      add = `${add}${options.addition}${options.additionSeparator}`
+    }
+  }
+
+  add = add.slice(0, add.length - options.additionSeparator.length)
+    
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result = `${result}${str}${add}${options.separator}`
+  }
+  result = result.slice(0, result.length - options.separator.length)
+
+  return result
 }
 
 module.exports = {
